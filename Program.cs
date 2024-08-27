aabfcd011abc98c860e8649c301426f6b96793f5
BelCoelho <isabela.coelho213@gmail.com>
2024-08-27 13:49:11 -0300

    Finalizado (facinho)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "0987654321", modelo: "Nokinha", imei: "0987", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Buble Witch Saga\n\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "123456789", modelo: "Iphonezinho", imei: "1234", memoria: 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Candy Crash");
